2f1296a9ce36cc3a3c997302c395fc7016e11016
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-04 12:08:52 -0700

    Add MongoDB connection via Mongoose. Add ability to pull up DB on localhost or Heroku.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var handler = ('./lib/request-handler');
 var app = express();
 
-app.set('port', (process.env.PORT || 3000));
+var databaseLoc =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/shortlydb';
 
-app.listen(app.get('port'), function() {
-  console.log('Server now listening at port: ', app.get('port'));
+mongoose.connect(databaseLoc);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('Server now listening at port: ', port);
 });
 
 app.get('/*', function(req, res) {
   res.send('Hello world!');
 });
+
+module.exports = app;
