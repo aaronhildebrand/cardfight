283d96b92bbca3f3a3c3aa0863f541d556e8e48e
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-05 09:01:45 -0700

    Update game-handler.js to properly send cards when user draws cards.

--- server/game/game-handler.js
+++ server/game/game-handler.js
@@ -1,70 +1,37 @@
 var mongoose = require('mongoose');
 var Cards = require('../db/cards.js');
-// var Promise = require('bluebird');
+// var app = require('../server.js');
 
-// Promise.promisifyAll(mongoose);
-// mongoose.Promise = Promise;
-
-function drawCard(idx) {
-  var promise = Cards.find({index: idx}).exec();
+function drawCards(indexes) {
+  var promise = Cards.find({'index': { $in: indexes}}).exec();
   return promise;
 }
 
-var whiteCard = function() {
-  var idx =  Math.ceil(Math.random()*178);
-  var card = drawCard(idx);
+var cardIndexes = function(indexes, countWhite, countBlack) {
+  if(countWhite) {
+    for(var i = 0; i < countWhite; i++) {
+      indexes.push(Math.ceil(Math.random()*178));
+    }
+  }
+  if(countBlack) {
+    for(var j = 0; j < countBlack; j++) {
+      indexes.push(Math.ceil(Math.random()*322)+178);
+    }
+  }
+};
 
-  // Cards.find({'index': idx}).exec()
-  // // , function(err, card) {
-  //   // if(err) {
-  //   //   console.log('Error finding card', err);
-  //   // } else {
-  //   //   return card;
-  //   // }
-  // // })
-  card.then(function(result) {
-    console.log(result);
-    return result;
+exports.drawHand = function(req, res) {
+  var indexes = [];
+  cardIndexes(indexes, 1, 2);
+  console.log(indexes);
+
+  var cards = drawCards(indexes);
+
+  cards.then(function(hand) {
+    console.log(hand);
+    res.send(hand);
   })
   .catch(function(error) {
-    console.log('error', error);
+    console.log('Error', error);
   });
-  // console.log(card);
-};
-
-var blackCard = function() {
-  var idx = Math.ceil(Math.random()*322)+178;
-
-  var card = Cards.find({'index': idx}, 'index', function(err, result) {
-    if(err) {
-      console.log('Error finding card', err);
-    }
-    else {
-      console.log(err);
-      return result;
-    }
-  });
-};
-
-exports.drawHand = function() {
-  var hand = [];
-  hand.push(whiteCard());
-  hand.push(blackCard());
-  hand.push(blackCard());
-  // hand.push(blackCard(function(data) {
-  //   return(data);
-  // }));
-  // hand.push(blackCard(function(data) {
-  //   return(data);
-  // }));
-  // var card2 = blackCard(function(data) {
-  //   return(data);
-  // });
-  // var card3 = blackCard(function(data) {
-  //   return(data);
-  // });
-
-  console.log(data);
-
-  return hand;
 };
